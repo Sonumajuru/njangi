43d763c06e894539107af16c2b6f547ca2474dc9
Sonumajuru <sonumajuru@gmail.com>
2023-07-26 13:59:28 +0200

    Files

---
+++ src/main/java/com/genesistech/njangiApi/NjangiapiApplication.java
@@ -0,0 +1,13 @@
+package com.genesistech.njangiApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NjangiapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NjangiapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/genesistech/njangiApi/NjangiapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genesistech.njangiApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NjangiapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
